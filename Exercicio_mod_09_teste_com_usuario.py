197c6988eda303e68a64a546be3491019e73d6e0
lanaemvocacao <lanag2202@gmail.com>
2026-04-27 16:43:03 -0300

    upload

---
+++ Exercicio_mod_09_teste_com_usuario.py
@@ -0,0 +1,25 @@
+'''
+
+
+'''
+
+class Celular:
+    def __init__(self,marca,modelo):
+        self.marca = marca
+        self.modelo = modelo
+        self.bateria = 100
+    
+    def fazer_chamada(self,custo_bateria):
+        print(F"\n---Chamada no {self.modelo}---")
+        try:
+            self.bateria-=custo_bateria
+        except TypeError:
+            print("Erro: O custo da bateria deve ser um número!")
+        else:
+            print(f"Sucesso! Bateria atual: {self.bateria}%")
+        finally:
+            print("Sistema finalizado.")
+
+meu_celular = Celular("Sansung","S24")
+
+        
