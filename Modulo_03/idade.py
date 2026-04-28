1e864590df080388601c2b5b81ee18c1f571399e
lanaemvocacao <lanag2202@gmail.com>
2026-04-28 07:30:16 -0300

    upload

---
+++ Modulo_03/idade.py
@@ -0,0 +1,19 @@
+'''
+
+
+'''
+
+print('Digite sua idade : ')
+
+idade = int(input())
+if idade <= 11:
+    print('Usuário é criança')
+
+elif idade <= 17:   
+    print('Usuário é adolescente')
+
+elif idade <= 65:
+    print('Usuário é adulto')
+
+else:
+    print('Usuário é idoso ')
